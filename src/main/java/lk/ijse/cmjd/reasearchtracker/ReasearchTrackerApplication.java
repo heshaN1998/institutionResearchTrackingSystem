51347bdc4340d6851f4088e19822ce96a6eb1266
heshan <heshansandakelum2018@gmail.com>
2025-11-08 21:40:59 +0530

    update Application

--- src/main/java/lk/ijse/cmjd/reasearchtracker/ReasearchTrackerApplication.java
+++ src/main/java/lk/ijse/cmjd/reasearchtracker/ReasearchTrackerApplication.java
@@ -3,15 +3,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-
 @SpringBootApplication
 @EnableJpaAuditing
+
 public class ReasearchTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReasearchTrackerApplication.class, args);
 	}
         SpringApplication.run(ResearchTrackerApplication.class, args);
-
 
 }
